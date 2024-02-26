6e0fa721e1ac5bcb52c8b8584796923ed2dc4dc7
Michael Ly <108250510+MichaelLy766@users.noreply.github.com>
2024-02-26 15:19:16 -0800

    added comments

--- Graph.java
+++ Graph.java
@@ -1,6 +1,6 @@
 /**************************************************************/
 /* Michael Ly                                                 */
-/* Login ID: stud9999                                         */
+/* Login ID: 016185078                                        */
 /* CS 3310, Spring 2024                                       */
 /* Programming Assignment 1                                   */
 /* Graph class: defines graph and graph methods               */
@@ -10,15 +10,20 @@ import java.util.ArrayList;
 
 public class Graph {
     int numVertices;
-    ArrayList<ArrayList<Integer> > adjacencyList;
+    ArrayList<ArrayList<Integer>> adjacencyList;
     
     /**
      * Constructor: Graph
      * Purpose: Construct a new Graph object with number of vertices numVertices
-     * @param numVertices           number of vertices
+     * Parameters:
+     * @param numVertices          int: number of vertices
      */
     public Graph (int numVertices) {
+
+        // sets number of vertices
         this.numVertices = numVertices;
+
+        // creates new arraylist, add a new arraylist for each vertex in the graph
         adjacencyList = new ArrayList<>();
         for (int i = 0; i < numVertices; i++) {
             adjacencyList.add(i, new ArrayList<>());
@@ -27,12 +32,15 @@ public class Graph {
 
     /**
      * Method: addEdge
-     * Purpose: add an undirected edge to the adjecancy list storing the grpah's 
+     * Purpose: add an undirected edge to the adjecancy list storing the graph's 
      *          connections
-     * @param source            starting vertex of the edge
-     * @param destination       ending vertex of the edge
+     * Parameters:
+     * @param source            int: starting vertex of the edge
+     * @param destination       int: ending vertex of the edge
      */
     public void addEdge (int source, int destination) {
+
+        // add connection both ways because of undirected graph
         adjacencyList.get(source).add(destination);
         adjacencyList.get(destination).add(source);
     }
@@ -40,18 +48,22 @@ public class Graph {
     /**
      * Method: DFS
      * Purpose: tranverse the graph using depth-first search, adding visited
-     *          and connected nodes to Arraylist component
-     * Paremters: 
-     * @param component         ArrayList of connected vertices
-     * @param vertex            integer representing a vertex 
-     * @param visited           boolean represent visited or not
-     * @return
+     *          and connected vertices to the Arraylist component
+     * Parameters: 
+     * @param component         ArrayList<Integer>: of connected vertices
+     * @param vertex            int: representing a vertex 
+     * @param visited           boolean[]: representing if each vertex visited or not
+     * @return ArrayList<Integer>: a connected component of the graph
      */
     private ArrayList<Integer> DFS(ArrayList<Integer> component, int vertex, boolean[] visited) {
         visited[vertex] = true;
         
+        // add visited vertex to the arraylist component, to be return once tranversal finishes,
+        // using vertex + 1 because so vertices are display as indexed 1
         component.add(vertex + 1);
 
+        // for every vertex that the current vertex is connected to
+        // if it is unvisited, recursively visit with DFS
         for (int v : adjacencyList.get(vertex)) {
             if (!visited[v]) {
                 DFS(component, v, visited);
@@ -60,11 +72,23 @@ public class Graph {
         return component; 
     }
 
-    //TODO method header comments, 
+    /**
+     * Method: connectedComponents
+     * Purpose: to continue tranversing other components of the graph even if DFS finished tranversing
+     *          a particular component, keeps tracking of the connected components of the graph and 
+     *          returns it
+     * @return ArrayList<ArrayList<Integer>>: 2D arraylist of all connected components
+     */
     public ArrayList<ArrayList<Integer>> connectedComponents() {
-        boolean[] visited = new boolean[numVertices + 1];       //TODO comments explaining vars
+
+        // boolean array representing visit status for each vertex
+        boolean[] visited = new boolean[numVertices];       
+
+        // 2D arraylist of all connected components
         ArrayList<ArrayList<Integer>> connectedComponents = new ArrayList<>();
-        for (int vertex = 0; vertex < numVertices; vertex++) {          //TODO comments explaining taks
+
+        // for each vertex in graph, if it is unvisted, perform DFS 
+        for (int vertex = 0; vertex < numVertices; vertex++) {
             if (!visited[vertex]) {
                 ArrayList<Integer> component = new ArrayList<>();
                 connectedComponents.add(DFS(component, vertex, visited));
--- Prog1.java
+++ Prog1.java
@@ -1,6 +1,6 @@
 /**************************************************************/
 /* Michael Ly                                                 */
-/* Login ID: stud9999                                         */
+/* Login ID: 016185078                                        */
 /* CS 3310, Spring 2024                                       */
 /* Programming Assignment 1                                   */
 /* Prog1 class: driver program                                */
@@ -20,32 +20,34 @@ public class Prog1 {
         try {
             Scanner fileScanner = new Scanner(inputFile);
 
-            int numVertices;            // TODO: comments all vars with purpose
-            Graph graph;
-            String edge;
-            String[] tokens;
-            int graphCounter = 0; 
+            int numVertices;            // used to track number of vertices for each graph            
+            Graph graph;                // used as the variable for graph objects
+            String edge;                // used as variable for edges 
+            String[] tokens;            // used as variable for tokens after splitting edges
+            int graphCounter = 0;       // counts graphs number, used for displaying
+            ArrayList<ArrayList<Integer>> connectedComponents;      // variable used for print connected components
+            int numCC;                  // variable used for print number of connected components
 
-            while (fileScanner.hasNextLine()) {     // TODO: inline comments about task performed
+            while (fileScanner.hasNextLine()) {
                 
                 graphCounter++;
                 String line = fileScanner.nextLine();
                 Scanner lineScanner = new Scanner(line);
 
                 numVertices = Integer.parseInt(lineScanner.next());     
-                graph = new Graph(numVertices); 
+                graph = new Graph(numVertices);     // create new graph with numVertices vertices
 
-                while (lineScanner.hasNext()) {
-                    edge = lineScanner.next();
-                    edge = edge.replace("(", "");
+                while (lineScanner.hasNext()) {     // processed edges from input file
+                    edge = lineScanner.next();                     
+                    edge = edge.replace("(", "");       
                     edge = edge.replace(")", "");
-                    tokens = edge.split(",");
-                    graph.addEdge(Integer.parseInt(tokens[0]) - 1, Integer.parseInt(tokens[1]) - 1); 
+                    tokens = edge.split(",");                        
+                    graph.addEdge(Integer.parseInt(tokens[0]) - 1, Integer.parseInt(tokens[1]) - 1); // add edges to graph
                 }
-                ArrayList<ArrayList<Integer>> connectedComponents = new ArrayList<>();
-                connectedComponents = graph.connectedComponents();
-                int numCC = connectedComponents.size();
-                System.out.println("Graph" + graphCounter + ":");
+                connectedComponents = new ArrayList<>();
+                connectedComponents = graph.connectedComponents();          // gets list of connected components
+                numCC = connectedComponents.size();                         // gets number of connected components
+                System.out.println("Graph" + graphCounter + ":");           // display to stdout
                 System.out.println(numCC + " Connected Components: " + connectedComponents + "\n");
 
                 lineScanner.close();
